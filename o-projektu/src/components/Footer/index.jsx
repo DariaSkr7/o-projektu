a8ddc4fc021e7d95924c282e44554947460587d8
Daria Skoryk <dariaskoryk@gmail.com>
2023-11-11 20:23:15 +0100

    Header + Footer

--- o-projektu/src/components/Footer/index.jsx
+++ o-projektu/src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer>
       <p>
-        <span>2023</span> Daria Skoryk and Anna Švejdová
+        <span>2023 |</span> Daria Skoryk and Anna Švejdová
       </p>
     </footer>
   );
