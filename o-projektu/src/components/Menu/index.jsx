2bf87054de7a56e8d40f1632937f32943ea368e8
Daria Skoryk <dariaskoryk@gmail.com>
2023-11-12 21:18:53 +0100

    Fix header bug

--- o-projektu/src/components/Menu/index.jsx
+++ o-projektu/src/components/Menu/index.jsx
@@ -1,17 +1,28 @@
 import './style.css';
 
 export const Menu = () => {
+  const onLinkClick = () => {
+    // fix link click bug
+    window.scrollTo({ top: window.top });
+  };
+
   return (
     <nav className="navbar">
       <ul>
         <li>
-          <a href="#project">Project</a>
+          <a onClick={onLinkClick} href="#project">
+            Project
+          </a>
         </li>
         <li>
-          <a href="#anna">Anna</a>
+          <a onClick={onLinkClick} href="#anna">
+            Anna
+          </a>
         </li>
         <li>
-          <a href="#daria">Daria</a>
+          <a onClick={onLinkClick} href="#daria">
+            Daria
+          </a>
         </li>
       </ul>
     </nav>
